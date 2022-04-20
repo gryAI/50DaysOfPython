ec653143153bca46e16d62cef9aaf33f350faed1
Lumine_ <70693859+InsightInspector@users.noreply.github.com>
2022-04-21 01:05:27 +0800

    Updated docstrings

--- Day 2 - convert_add.py
+++ Day 2 - convert_add.py
@@ -1,13 +1,21 @@
 """
 Created on Thu Apr 21 12:08:31 2022
 @author: LUMINE
+
+Day 2 50 Days of Python
+
+Write a function called convert_add that takes a list of strings as an argument and
+converts it to integers and sums the list.
+
+For example, [‘1’, ‘3’, ‘5’] should be converted to [1, 3, 5] and summed to 9.
+
 """
 
-# Define the function
+# Define the convert_add function
 def convert_add(num):
     """
     Takes a list of strings as an argument and converts it to integers and sums the list.
-    :param args: list(str): list of strings
+    :param num(list): list of strings
     :return: int
     """
     int_list = [int(x) for x in num]
